c6eb7c2e19ffcc1e74b51147446b2db432442f91
str1ngs <42488000+str1ngs@users.noreply.github.com>
2023-04-06 12:53:06 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Lemmik from './Lemmik.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lemmikraamatud :</h1>
+      <Lemmik />
+    </div>
+  );
+}
+
+export default App;
